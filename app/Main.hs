f76cfc509cb111569f2be21e37928ac49b7147fb
Ant <ryu4979@gmail.com>
2021-04-03 22:45:40 +0900

    feat: init

---
+++ app/Main.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Network.Wai              as Wai
+import qualified Network.HTTP.Types       as HTypes
+
+main :: IO ()
+main = Warp.run 8000 helloApp
+
+helloApp :: Wai.Application
+helloApp req send = send $ Wai.responseBuilder HTypes.status200 [] "Welcome Haskell Web Page(´・ω・｀)"
